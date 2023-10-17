d12c992daf8d384297b1e195c1a451d952e8305f
asrani <asrani.27@gmail.com>
2023-10-17 12:21:11 +0800

    f

--- resources/views/pegawai/sk/pkkk.blade.php
+++ resources/views/pegawai/sk/pkkk.blade.php
@@ -24,4 +24,10 @@
         <input type="text" class="form-control" readonly value="{{$data->sk_cpns}}">
       </div>
     </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">File SK </label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" readonly value="{{$data->sk_cpns}}">
+      </div>
+    </div>
 </form>
