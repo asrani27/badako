d966d60f9354b2149d60b35a2446fdd2e8981b9a
asrani <asrani.27@gmail.com>
2023-10-27 21:29:01 +0800

    f

--- resources/views/pegawai/sk/pns.blade.php
+++ resources/views/pegawai/sk/pns.blade.php
@@ -176,5 +176,54 @@
         <a href="/storage/{{Auth::user()->pegawai->nip}}/kepegawaian/{{Auth::user()->pegawai->file_berkala}}" class="btn btn-primary btn-xs" target="_blank"><i class="fa fa-download"></i> Download Jafung</a>
       </div>
     </div>
+    <hr> 
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label">SK STR</label>
+      <div class="col-sm-9">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">Nomor</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" readonly value="{{$data->nomor_str}}">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">Tanggal </label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" readonly value="{{$data->tanggal_str}}">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">File SK STR </label>
+      <div class="col-sm-9">
+        <a href="/storage/{{Auth::user()->pegawai->nip}}/kepegawaian/{{Auth::user()->pegawai->file_str}}" class="btn btn-primary btn-xs" target="_blank"><i class="fa fa-download"></i> Download Jafung</a>
+      </div>
+    </div> 
     <hr>
+
+    <div class="form-group">
+      <label class="col-sm-3 control-label">SK SIP</label>
+      <div class="col-sm-9">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">Nomor</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" readonly value="{{$data->nomor_sip}}">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">Tanggal </label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" readonly value="{{$data->tanggal_sip}}">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">File SIP </label>
+      <div class="col-sm-9">
+        <a href="/storage/{{Auth::user()->pegawai->nip}}/kepegawaian/{{Auth::user()->pegawai->file_sip}}" class="btn btn-primary btn-xs" target="_blank"><i class="fa fa-download"></i> Download Jafung</a>
+      </div>
+    </div>
   </form>
