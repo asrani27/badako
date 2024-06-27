5e4a634886a439bc76a4ddf749a7ffa0f03af861
asrani <asrani.27@gmail.com>
2024-06-27 16:11:45 +0800

    f

--- app/Http/Controllers/CutiController.php
+++ app/Http/Controllers/CutiController.php
@@ -85,7 +85,7 @@ class CutiController extends Controller
         //     $data = Cuti::where('sekretaris', Auth::user()->pegawai->nip)->get();
         //     return view('pegawai.cuti.verifikasi', compact('data'));
         // } else {
-        $data = Cuti::where('atasan_langsung', Auth::user()->pegawai->nip)->paginate(15);
+        $data = Cuti::where('atasan_langsung', Auth::user()->pegawai->nip)->orderBy('verifikasi_atasan', 'ASC')->paginate(15);
         return view('pegawai.cuti.verifikasi', compact('data'));
         //}
 
